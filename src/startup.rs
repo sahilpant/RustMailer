abfc207749d6dbe8bb691ba489811f9c4a95e9fe
sahilpant <sahilpant16@gmail.com>
2025-12-26 16:08:46 +0530

    added logger

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
-
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{App, HttpServer, dev::Server, web, middleware::Logger};
+use env_logger::Env;
 use sqlx::PgPool;
 
 use crate::routes::{health_check, subscribe};
@@ -9,9 +9,11 @@ pub fn run(
     listner:TcpListener,
     db_pool: PgPool
 ) -> Result<Server,std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
